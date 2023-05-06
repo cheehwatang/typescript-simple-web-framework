1e7436d034b82c990bed1bfc43f8b38a04293bce
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-06 16:08:32 +0800

    Adds save method to the User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -47,6 +47,16 @@ class User {
         this.set(response.data);
       });
   }
+
+  save(): void {
+    const id = this.get("id");
+
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post("http://localhost:3000/users", this.data);
+    }
+  }
 }
 
 export { User };
