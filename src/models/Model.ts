81242946660827dee2750218820c1f6c165f628b
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-07 14:08:51 +0800

    Adds Model class and interface for ModelAttributes, Sync and Events

---
+++ src/models/Model.ts
@@ -0,0 +1,21 @@
+import { AxiosPromise } from "axios";
+
+interface ModelAttributes<T> {
+  set(value: T): void;
+  get<K extends keyof T>(key: K): T[K];
+  getAll(): T;
+}
+
+interface Sync<T> {
+  fetch(id: number): AxiosPromise<T>;
+  save(data: T): AxiosPromise<T>;
+}
+
+interface Events {
+  on(eventName: string, callback: () => void): void;
+  trigger(eventName: string): void;
+}
+
+class Model {}
+
+export { Model, ModelAttributes, Sync, Events };
