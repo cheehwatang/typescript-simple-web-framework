dfea12f380ba3d3cd9e18f3b6d6bcea3bd7e6f98
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-07 13:58:21 +0800

    Make the events, sync and attributes field private

--- src/models/User.ts
+++ src/models/User.ts
@@ -12,9 +12,9 @@ interface UserProps {
 const rootUrl = "http://localhost:3000/users";
 
 class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
+  private events: Eventing = new Eventing();
+  private sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  private attributes: Attributes<UserProps>;
 
   constructor(private data: UserProps) {
     this.attributes = new Attributes<UserProps>(data);
