3631fc80387a6e48e0c97a687835dbdf68fe2621
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-08 15:55:20 +0800

    Adds set name functionality to the user form

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { User } from "./models/User";
 
 const user = User.build({ name: "Alice", age: 50 });
 
-const userForm = new UserForm(document.getElementById("root")!, user);
+const userForm = new UserForm(
+  document.getElementById("root") as HTMLElement,
+  user
+);
 
 userForm.render();
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -17,8 +17,8 @@ class UserForm {
       <h1>User Form</h1>
       <div>User name: ${this.model.get("name")}</div>
       <div>User age: ${this.model.get("age")}</div>
-      <input />
-      <button>Submit</button>
+      <input id="name-input" placeholder="Enter new name..." />
+      <button class="set-name">Set Name</button>
       <button class="set-age">Set Random Age</button>
     </div>
     `;
@@ -27,11 +27,19 @@ class UserForm {
   eventsMap = (): { [key: string]: () => void } => {
     return {
       "click:.set-age": this.onSetAgeClick,
+      "click:.set-name": this.onSetNameClick,
     };
   };
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
+  };
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector("#name-input") as HTMLInputElement;
+    const name = input.value;
+
+    this.model.set({ name });
   };
 
   bindEvents = (fragment: DocumentFragment): void => {
