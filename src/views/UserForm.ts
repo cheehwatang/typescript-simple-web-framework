725fb81facead65f02c9ebac10b5c1f5b88f5c2e
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-08 15:11:26 +0800

    Adds event mapping and binding of events to elements in the user form

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,21 +1,53 @@
+import { log } from "console";
+
 class UserForm {
   constructor(public parent: Element) {}
 
-  template(): string {
+  template = (): string => {
     return `
-      <div>
-        <h1>User Form</h1>
-        <input />
-      </div>
+    <div>
+    <h1>User Form</h1>
+    <input />
+    <button>Submit</button>
+    </div>
     `;
-  }
+  };
 
-  render(): void {
+  eventsMap = (): { [key: string]: () => void } => {
+    return {
+      "click:button": this.onButtonClick,
+      "mouseover:h1": this.onHeaderMouseover,
+    };
+  };
+
+  onButtonClick = (): void => {
+    console.log("Submitted");
+  };
+
+  onHeaderMouseover = (): void => {
+    console.log("h1 element hovered");
+  };
+
+  bindEvents = (fragment: DocumentFragment): void => {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  };
+
+  render = (): void => {
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
 
+    this.bindEvents(templateElement.content);
+
     this.parent.append(templateElement.content);
-  }
+  };
 }
 
 export { UserForm };
