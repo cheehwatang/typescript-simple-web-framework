d28802275f589f85dba66f48b1a78fdd67a68022
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-08 15:45:02 +0800

    Adds ability for the user form to re-render

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,7 +1,15 @@
 import { User } from "../models/User";
 
 class UserForm {
-  constructor(public parent: Element, public model: User) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel = (): void => {
+    this.model.on("change", () => {
+      this.render();
+    });
+  };
 
   template = (): string => {
     return `
@@ -39,6 +47,8 @@ class UserForm {
   };
 
   render = (): void => {
+    this.parent.innerHTML = "";
+
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
 
