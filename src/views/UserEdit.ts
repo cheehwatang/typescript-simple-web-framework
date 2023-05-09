0f6c40868f677ba1849e56663df002e88ed440b3
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-09 16:11:26 +0800

    Use nesting for UserEdit to render the regions

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -1,5 +1,7 @@
 import { View } from "./View";
 import { User, UserProps } from "../models/User";
+import { UserShow } from "./UserShow";
+import { UserForm } from "./UserForm";
 
 class UserEdit extends View<User, UserProps> {
   template = (): string => {
@@ -9,6 +11,18 @@ class UserEdit extends View<User, UserProps> {
         <div class="user-form"></div>
       </div>
     `;
+  };
+
+  regionsMap = (): { [key: string]: string } => {
+    return {
+      userForm: ".user-form",
+      userShow: ".user-show",
+    };
+  };
+
+  onRender = (): void => {
+    new UserShow(this.regions.userShow, this.model).render();
+    new UserForm(this.regions.userForm, this.model).render();
   };
 }
 
